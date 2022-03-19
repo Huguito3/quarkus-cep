186817c7a0cf1cbd2343612cfb8a8ffce29d99b2
Hugo D. I <hduraniroz@gmail.com>
2022-03-19 20:05:32 -0300

    agregado tolerancia a falhas - monitoreamento

--- quarkus-customers/src/main/java/br/com/impacta/quarkus/CustomerResource.java
+++ quarkus-customers/src/main/java/br/com/impacta/quarkus/CustomerResource.java
@@ -1,14 +1,40 @@
 package br.com.impacta.quarkus;
 
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.ArrayList;
+
+import java.time.temporal.ChronoUnit;
 
 @Path("/customers")
 public class CustomerResource {
+
+    private static final Logger LOGGER = Logger.getLogger(CustomerResource.class.toString());
+
+    @ConfigProperty(name = "isTestingFault")
+    boolean isTestingFault;
+
+    @ConfigProperty(name = "isRetry")
+    boolean isRetry;
+    final int quantidadeRetry = 5;
+    int exceptionCount = quantidadeRetry - 1;
+    int count = 0;
+
+    @ConfigProperty(name = "isFallBack")
+    boolean isFallBack;
 
     @Inject
     CustomerService customerService;
@@ -19,7 +45,15 @@ public class CustomerResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = quantidadeRetry, delay = 1, delayUnit = ChronoUnit.SECONDS)
+    @Fallback(fallbackMethod = "fallbackCustomers")
     public List<Customer> listCustomer() {
+        // LOGGER.info("listCustomer");
+        System.out.println("Lista Customers");
+        System.out.println(MessageFormat.format("isTestingFault {0}", isTestingFault));
+        if (isTestingFault) {
+            executeFault();
+        }
         return customerService.listCustomer();
     }
 
@@ -73,7 +107,6 @@ public class CustomerResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Customer addCustomer(Customer customer) {
-        System.out.println("AddCustomer");
         customer.setNumeroCep(buscaCepRestClient.getNumeroCEP().getNumeroCep());
         Customer customerEntity = customerService.addCustomer(customer);
         return customerEntity;
@@ -97,6 +130,30 @@ public class CustomerResource {
         customerEntity = customerService.getCustomerByRg(customerEntity);
         customerEntity = customerService.deleteCustomer(customerEntity);
         return customerEntity;
+    }
+
+    @Gauge(name = "QUARKUS_QUANTIDADE_CLIENTES", unit = MetricUnits.NONE, description = "QUANTIDADE DE CLIENTES")
+    public long checkCustomerAmmout() {
+        return customerService.listCustomer().size();
+    }
+
+    private void executeFault() {
+        if (isRetry) {
+            while (count < exceptionCount) {
+                count++;
+                String message = "Simulando Erro: " + count;
+                LOGGER.log(Level.SEVERE, message);
+                throw new RuntimeException(message);
+            }
+            count = 0;
+        }
+        if (isFallBack) {
+            throw new RuntimeException("Simulando Erro: ");
+        }
+    }
+
+    private List<Customer> fallbackCustomers() {
+        return new ArrayList<Customer>(0);
     }
 
 }
